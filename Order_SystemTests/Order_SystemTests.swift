a325315899a59604d7a5a4fa2cff5caa4a9abac6
wx_shadow <wx_shadow@wx-shadowdeiMac.local>
2017-08-12 14:48:37 +0800

    Initial Commit

---
+++ Order_SystemTests/Order_SystemTests.swift
@@ -0,0 +1,36 @@
+//
+//  Order_SystemTests.swift
+//  Order_SystemTests
+//
+//  Created by wx_shadow on 2017/8/12.
+//  Copyright © 2017年 wx_shadow. All rights reserved.
+//
+
+import XCTest
+@testable import Order_System
+
+class Order_SystemTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
