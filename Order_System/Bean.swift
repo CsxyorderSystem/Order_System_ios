db978b7e58773745f6158c712c363ca6be88b7fc
wx_shadow <wx_shadow@wx-shadowdeiMac.local>
2017-08-14 20:55:04 +0800

    v0.02 开始添加Bean

---
+++ Order_System/Bean.swift
@@ -0,0 +1,18 @@
+//
+//  Bean.swift
+//  Order_System
+//
+//  Created by wx_shadow on 2017/8/14.
+//  Copyright © 2017年 wx_shadow. All rights reserved.
+//
+
+import Foundation
+
+class Address_Bean  {
+    var A_id : Int! ;
+    var A_street : String = "";
+    var A_province : String = "";
+    var A_city : String = "";
+    var A_district : String = "";
+}
+
